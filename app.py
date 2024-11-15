e052812a9dae18b7d109a8e4ee1a708f45ad952f
Tung Nguyen Tu <tungnguyentu@bizflycloud.vn>
2024-11-15 15:32:54 +0700

    add req

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def rsvp():
     return jsonify({'message': 'RSVP received', 'data': data}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5004)
+    app.run(debug=True, port=5000)
